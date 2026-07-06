78f84220d2d6cdd5c2e8afba8a926aef193ddf77
53494 <152260571+53494@users.noreply.github.com>
2026-07-06 11:41:58 +0800

    Add files via upload
    更新打包dist，适配GitHub Pages部署路径

--- dist/registerSW.js
+++ dist/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/recollection-vocab-pwa/sw.js', { scope: '/recollection-vocab-pwa/' })})}
